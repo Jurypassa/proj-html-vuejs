71e5c786ea590ff20c6c8ec74a616068d1615995
Jury <jurypassa@gmail.com>
2022-01-11 16:16:42 +0100

    navbar, hero

---
+++ src/components/elements/Barra.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav id="navbar">
+    <h4>home</h4>
+    <h4>the club</h4>
+    <h4>events</h4>
+    <img src="../../assets/img/avada-nightclub-logo-2x-400x132.png" alt="logo">
+    <h4>jobs</h4>
+    <h4>news</h4>
+    <h4>contact us</h4>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: "Barra"
+}
+</script>
+
+<style lang="scss" scoped>
+  h4{
+    display: inline-block;
+    text-transform: uppercase;
+    margin-top: 50px;
+    font-weight: bold;
+  };
+
+  #navbar{
+    height: 132px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,15 +1,50 @@
 <template>
-  <header>
-      
+  <header id="header">
+
+      <div class="container">
+          
+      <!-- inizio sezione navbar -->
+        <div class="nav">
+            <Barra/>
+        </div>
+      <!-- fine sezione navbar -->
+
+      <!-- inizio sezione Hero -->
+      <div class="hero">
+          <img src="../../assets/img/avada-nightclub-homepage-image.png" alt="home page image">
+      </div>
+      <!-- fine sezione Hero -->
+
+
+
+      </div>
   </header>
 </template>
 
 <script>
+import Barra from "../elements/Barra.vue"
+
 export default {
     name: "Header",
+    components: {
+        Barra,
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+    #header{
+        height: 1300px;
+        background-image: url(../../assets/img/avada-nightclub-home-hero.jpg);
+        background-size: cover;
+    };
 
+    .nav{
+        padding-top: 20px;
+    };
+
+    .hero img{
+        width: 100%;
+        padding-top: 50px;
+    }
 </style>
